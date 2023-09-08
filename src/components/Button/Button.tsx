9ca2f0258c2f3d0050a8a727a507de6d7aa05c1c
Shorax <60271516+shorax@users.noreply.github.com>
2023-09-08 18:22:04 +0200

    Initial commit with button as test

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export interface ButtonProps {
+    label: string;
+    onClick?: () => void;
+}
+
+const Button = (props: ButtonProps) => {
+    return (
+        <button onClick={props.onClick}>
+            {props.label}
+        </button>
+    );
+}
+
+export default Button;
