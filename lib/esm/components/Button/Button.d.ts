1be8a47e275bebbe54bc8a330602fa934b467437
Shorax <60271516+shorax@users.noreply.github.com>
2023-11-11 17:28:19 +0100

    add button & dark mdoe context

--- lib/esm/components/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export interface ButtonProps {
-    label: string;
-    onClick?: () => void;
-}
-declare const Button: (props: ButtonProps) => React.JSX.Element;
-export default Button;
